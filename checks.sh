e056d9188fb9f4c78380ddac9ea357f57ecb6317
knuxfanwin8 <kfw8.mail@gmail.com>
2018-11-16 06:39:02 +0100

    Fix bored throwing errors at firsrt startup (caused by missing .bored dir)

--- checks.sh
+++ checks.sh
@@ -1,3 +1,4 @@
+if [ ! -d ~/.bored ]; then boreddebug "making .bored dir" && mkdir ~/.bored; fi
 if [ ! -e $list ]; then boreddebug "creating activity list" && cp idealist.txt ~/.bored/ && chmod 777 $list; fi 
 if [ ! -e $counterfile ]; then boreddebug "creating counter" && echo "0" > $counterfile && chmod 777 $counterfile; fi
 if [ ! -r $list ] || [ ! -w $list ]; then boreddebug "fixing list perms" && chmod 777 $list; fi 
