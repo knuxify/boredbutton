baa6dfc23b393a233c58fc38f7368c31660b1121
knuxfanwin8 <kfw8.mail@gmail.com>
2018-11-16 06:34:14 +0100

    Update the UI to contain new options

--- boredsetup.sh
+++ boredsetup.sh
@@ -2,7 +2,7 @@ boreddebug "Starting setup utility..."
 while true
 do
     clear
-	echo -e "${white}You used the 'I'm bored' button ${warn}$counter${white} times."
+	echo -e "${white}You used the button ${warn}$counter${white} times."
 	echo -e "\n${menuname}### Counter options ###${white}\n"
 	echo -e "${menuname}Remove counter data${white} - ${warn}del${white}."
 	echo -e "${menuname}Decrease counter by 1${white} - ${warn}decr${white}."
@@ -10,7 +10,8 @@ do
 	echo -e "\n${menuname}### Idea options ###${white}\n"
 	echo -e "${menuname}Remove idea data${white} - ${warn}actdel${white}."
 	echo -e "${menuname}Create a new idea${white} - ${warn}actnew${white}."
-	echo -e "${menuname}Print all activities${white} - ${warn}printact${white}."
+	echo -e "${menuname}Print all ideas${white} - ${warn}printact${white}."
+	echo -e "${menuname}Package idea list for distribution${white} - ${warn}pack${white}"
     echo -e "\n${menuname}Note: ${white}To recreate idea data, run ${warn}bored${white}."
     echo -e "\n${menuname}### Exit ###${white}\n"
 	echo -e "To exit, type anything else or just press Enter."
