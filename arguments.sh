29847a4a39ba30890ee342ac7e08bfe2e4fc6dc4
knuxfanwin8 <knuxfanwin8.mail@gmail.com>
2018-12-17 17:19:27 +0100

    Add idea manager argument

--- arguments.sh
+++ arguments.sh
@@ -1,6 +1,7 @@
 if [ "$arg" = "--about" ] || [ "$arg2" = "--about" ]; then aboutb && exit; fi
 if [ "$arg" = "--debug" ] || [ "$arg2" = "--debug" ]; then debug=true; else debug=false; fi 
 if [ "$arg" = "setup" ] || [ "$arg2" = "setup" ]; then setup; fi
+if [ "$arg" = "ideaman" ] || [ "$arg2" = "ideaman" ]; then ideaman; fi
 lookfor=f$arg
 lookfor2=f$arg2
 if [ "$(type -t $lookfor)" = "function" ]; then $lookfor; fi
